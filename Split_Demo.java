f1ae0135749bd2083972320fa71b5a7e0a1d0931
Bhushan43 <42267707+Bhushan43@users.noreply.github.com>
2018-08-10 15:30:30 +0530

    Add files via upload

---
+++ Split_Demo.java
@@ -0,0 +1,13 @@
+package stringOpertions;
+
+public class Split_Demo {
+	
+
+		public static void main(String[] args) {
+		 System.out.println("Program Arguments:");
+         for (String arg : args) {
+             System.out.println("" + arg);
+         }
+	}
+
+}
